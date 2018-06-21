49f85a3779a87f73c30ff765ac984c8215903dfd
apoorvaprabhu90 <40012513+apoorvaprabhu90@users.noreply.github.com>
2018-06-22 09:07:53 +1000

    Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,28 +1,26 @@
-# Plot 1 gives the data of Global Active power (kilowatts) which gives the household global minute-averaged active power (in kilowatt) from the UC Irvine Machine Learning Repository
+# Plot 3 is the plot of Energy sub-metering vs date/time of the household power consumption data taken from the UC Irvine Machine Learning Repository
 
 # The first step is to read the file
 datafile <- "C:/Users/apoor/OneDrive/Desktop/household_power_consumption.txt"
-data <- read.table(datafile, header = TRUE, 
+powerdata <- read.table(datafile, header = TRUE, 
                    sep = ";", stringsAsFactors = FALSE)
-                   
-# Convverting the date variables into the date format
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
-# The data is then subsetted by filtering the dates 2007-02-01 & 2007-02-02 only)
-subpowerdata <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+# Subsetting the file to filter out the dates, changing the date & time variables from chars to objects
+subpowerdata <- subset(powerdata,powerdata$Date=="1/2/2007" | powerdata$Date =="2/2/2007")
+subpowerdata$Date <- as.Date(subpowerdata$Date, format="%d/%m/%Y")
+subpowerdata$Time <- strptime(subpowerdata$Time, format="%H:%M:%S")
+subpowerdata[1:1440,"Time"] <- format(subpowerdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpowerdata[1441:2880,"Time"] <- format(subpowerdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-# Converting the dates & times
-data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+# Generating the plot with three different lines
+plot(subpowerdata$Time,subpowerdata$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpowerdata,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpowerdata,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpowerdata,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-# Generating the plot
-attach(data)
-plot(Sub_metering_1 ~ datetime, type = "l", 
-     ylab = "Energy sub metering", xlab = "")
-lines(Sub_metering_2 ~ datetime, col = "Red")
-lines(Sub_metering_3 ~ datetime, col = "Blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png, file = "plot3.png", height = 480, width = 480)
+# Saving the plot to the file
+dev.copy(png, file = "plot3.png")
 dev.off()
-detach(data)
+
+       
